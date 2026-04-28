2f80e308d4ee8ee7b6876238616b3635c9250c59
João Pedro Pratavieira da Silva <joaopedro19042006@gmail.com>
2026-04-27 20:48:45 -0400

    removendo resposta de mensagem em auth_service.dart

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -5,13 +5,10 @@ import 'package:flutter/foundation.dart';
 class AuthService {
   final String urlBase;
 
+  // ip da máquina onde está rodando o laravel
   AuthService({this.urlBase = 'http://127.0.0.1:8000/api/v1'});
 
   Future<bool> login(String email, String password) async {
-    debugPrint('--- INICIANDO REQUISIÇÃO DE LOGIN ---');
-    debugPrint('URL: $urlBase/login');
-    debugPrint('Dados enviados: Email: $email | Senha: [OCULTA]');
-
     try {
       final resposta = await http.post(
         Uri.parse('$urlBase/login'),
@@ -25,15 +22,9 @@ class AuthService {
         }),
       );
 
-      debugPrint('Status Code recebido: ${resposta.statusCode}');
-      debugPrint('Corpo da Resposta: ${resposta.body}');
-
       if (resposta.statusCode == 200) {
-        final dados = jsonDecode(resposta.body);
-        debugPrint('Token recebido com sucesso: ${dados['token']}');
         return true;
       } else {
-        debugPrint('Falha no login. Credenciais incorretas ou erro no backend.');
         return false;
       }
     } catch (e) {
